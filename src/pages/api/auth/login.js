33c07829312f191a9c4a3511f868745ccc806855
shaffanmohd <shaffanmohd@gmail.com>
2024-03-18 12:20:02 +0800

    wip

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -1,5 +1,7 @@
 import { login } from "@/controllers/auth";
 
 export default async function handler(req, res) {
-   if (req.method === "POST") return login(req, res);
+  if (req.method === "POST") {
+    return login(req, res);
+  }
 }
--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -1,5 +1,7 @@
 import { logout } from "@/controllers/auth";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") return logout(req, res);
+  if (req.method === "GET") {
+    return logout(req, res);
+  }
 }
