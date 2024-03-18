c45a028bbd921c3dadd85ad4288a539a386f482c
shaffanmohd <shaffanmohd@gmail.com>
2024-03-18 14:43:49 +0800

    debugging pt 5

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -56,34 +56,36 @@ export const signUp = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    console.log("🚀 ~ login ~ email:", req.body);
+ if (req.method === "POST") {
+   try {
+     await connectToMongoDB();
+     const { email, password } = req.body;
+     console.log("🚀 ~ login ~ email:", req.body);
 
-    await connectToMongoDB();
-    const user = await User.findOne({ email });
-    console.log("🚀 ~ login ~ user:", user);
+     const user = await User.findOne({ email });
+     console.log("🚀 ~ login ~ user:", user);
 
-    if (user) {
-      const isPasswordValid = await compare(password, user.password);
-      if (isPasswordValid) {
-        // const token = await createToken(user._id);
-        res.status(200).json({
-          message: "successful login",
-          data: {
-            id: user.id,
-            email: user.email,
-            // token,
-          },
-        });
-      }
-      throw error("Incorrect password/email");
-    }
-    throw error("Incorrect password/email");
-  } catch (err) {
-    const errors = handleError(err);
-    res.status(500).json({ errors });
-  }
+     if (user) {
+       const isPasswordValid = await compare(password, user.password);
+       if (isPasswordValid) {
+         // const token = await createToken(user._id);
+         res.status(200).json({
+           message: "successful login",
+           data: {
+             id: user.id,
+             email: user.email,
+             // token,
+           },
+         });
+       }
+       throw error("Incorrect password/email");
+     }
+     throw error("Incorrect password/email");
+   } catch (err) {
+     const errors = handleError(err);
+     res.status(500).json({ errors });
+   }
+ }
 };
 export const logout = async (req, res) => {
   try {
--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -4,34 +4,5 @@ import User from "@/models/user";
 import { compare } from "bcrypt";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      await connectToMongoDB();
-      const { email, password } = req.body;
-      console.log("🚀 ~ login ~ email:", req.body);
-
-      const user = await User.findOne({ email });
-      console.log("🚀 ~ login ~ user:", user);
-
-      if (user) {
-        const isPasswordValid = await compare(password, user.password);
-        if (isPasswordValid) {
-          // const token = await createToken(user._id);
-          res.status(200).json({
-            message: "successful login",
-            data: {
-              id: user.id,
-              email: user.email,
-              // token,
-            },
-          });
-        }
-        throw error("Incorrect password/email");
-      }
-      throw error("Incorrect password/email");
-    } catch (err) {
-      const errors = handleError(err);
-      res.status(500).json({ errors });
-    }
-  }
+  return login(req, res);
 }
